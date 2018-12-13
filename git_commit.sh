7bfdb15f4cd6f96b24c08517b95af2dead4d57e3
root <root@localhost.localdomain>
2018-12-13 07:42:36 -0500

    Corregido fallo tonto

--- git_commit.sh
+++ git_commit.sh
@@ -2,5 +2,5 @@ git pull #bajar lo que hay en el git
 git add -A #los archivos cambiados los guarda para luego subirlos
 echo Introduce el commit a subir\: #el comentario de lo que voy a subir
 read commit
-git commit -a -m $commit  #genera una version nuevo con los cambios y el comentario que he puesto en commit
+git commit -a -m "$commit"  #genera una version nuevo con los cambios y el comentario que he puesto en commit
 git push #sube a git , introducir user y password
