7fdef815c888f7d8ac3a04ac95200f2e5876de48
JulenCestero <julencestero.s8@gmail.com>
2019-01-07 17:17:22 +0100

    Bug Fixing

--- FINAL/Servidor/Suggestion_box_webpage/login.php
+++ FINAL/Servidor/Suggestion_box_webpage/login.php
@@ -2,18 +2,18 @@
 session_start();
 $name = $password = "";
 if($_SERVER["REQUEST_METHOD"] == "POST"){
-	$user = test_input($_POST["user"]);
-	$password = hash("sha256", $_POST["password"]);
+	$user = hash("sha256", test_input($_POST["user"]));
+	$password = hash("sha256", test_input($_POST["password"]));
 	$conn = new mysqli('localhost', 'admin', 'Admin_Smart-cities4', 'Hogwarts');
 	if($conn->connect_error){
 		die("Connection failed: " . $conn->connect_error);
 	}
 	$conn->set_charset("utf8");
-	$sql = "SELECT password, id FROM usuarios WHERE username='" . $user . "'";
+	$sql = "SELECT password, id FROM users WHERE username='" . $user . "'";
 	$result = $conn->query($sql);
 	if($result->num_rows > 0){
 		$passwordRow = $result->fetch_assoc();
-		$myHashPass  = $passwordRow['Password'];
+		$myHashPass  = $passwordRow['password'];
 		if($myHashPass == $password){
 			$_SESSION['username'] = $user;
 			$_SESSION['password'] = $myHashPass;
@@ -22,10 +22,10 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 			else redirect("welcomeWizard.php");
 		}
 	}
-	redirect("login.html"); // Login fallido
+	//redirect("login.html"); // Login fallido
 }
 else{
-	redirect("login.html"); // Login fallido
+	//redirect("login.html"); // Login fallido
 }
 
 function redirect($url){
